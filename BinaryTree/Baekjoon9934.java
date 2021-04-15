effadd5ebce3675fb8b5efb726cdbb1123c5b625
Seunghoon Han <gooriiie@gmail.com>
2021-04-15 17:27:06 +0900

    백준-완전이진트리

--- BinaryTree/Baekjoon9934.java
+++ BinaryTree/Baekjoon9934.java
@@ -3,69 +3,62 @@ package BinaryTree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 
 public class Baekjoon9934 {
-
+	
+	static int k;
+	static ArrayList<ArrayList<Integer>> result;
+	static int[] arr;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+				
 		StringTokenizer st;
 		
-		int deep = Integer.parseInt(br.readLine());
-		int node = (int)Math.pow(2,deep) - 1;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		k = Integer.parseInt(br.readLine());	// k개의 줄에 걸쳐서 정답 출력
+
+		st = new StringTokenizer(br.readLine());	// 빌딩 방문순서 입력
 		
-		MyNode tree = null;
+		arr = new int[st.countTokens()];		// 방문순서 저장할 배열
 		
-		for(int i = 0; i < node; i++) {
-			if(i == 0) {
-				tree = new MyNode(i);
-			}
-			else {
-				insert(tree, i);	
-			}
-			
+		for(int i = 0; i < arr.length; i++) {	// arr 배열에 방문순서 저장
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		inorder(tree);
+		result = new ArrayList<ArrayList<Integer>>();
+		
+		for(int i = 0; i < k; i++) {
+			result.add(new ArrayList<Integer>());
+		}
+		
+		solve(0, arr.length - 1, 0);
+		
+		for(int i = 0; i < k; i++) {
+			for(int j = 0; j < result.get(i).size(); j++) {
+				System.out.print(result.get(i).get(j) +" ");
+			}
+			System.out.println();
+		}
+		
 		
 	}
 	
-	public static void insert(MyNode root, int key) {
-
-			if(root.left == null) {
-				root.left = new MyNode(key);
-			}
-			else if(root.right == null){
-				root.right = new MyNode(key);
-			}
-			else {
-				insert(root.left, key);
-			}
-	}
-	
-	public static void inorder(MyNode root) {
-		if(root.left != null) {
-			inorder(root.left);
+	public static void solve(int start, int end, int floor) {
+		
+		if(floor == k) {
+			return;
 		}
-		System.out.print(root.data +" ");
-		if(root.right != null) {
-			inorder(root.right);
-		}
-	}
-}
-
-
-
-class MyNode {
-
-	MyNode left;
-	MyNode right;
-	int data;
-
-	MyNode(int data){
-		this.data = data;
+		
+		int mid = (start + end) / 2;
+		
+		result.get(floor).add(arr[mid]);
+		
+		solve(start, mid-1, floor+1);
+		solve(mid+1, end, floor+1);
+		
 	}
 }

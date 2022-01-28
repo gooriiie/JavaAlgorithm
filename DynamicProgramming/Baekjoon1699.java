06afed184728c72220695c165907695e26ca114b
gooriiie <fkstldghks123@naver.com>
2022-01-28 16:57:56 +0900

    Dynamic Programming : BOJ1699

---
+++ DynamicProgramming/Baekjoon1699.java
@@ -0,0 +1,27 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon1699 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[n+1];
+		
+		for(int i = 1; i < n+1; i++) {
+			dp[i] = i;
+			for(int j = 1; j*j <= i; j++) {
+				dp[i] = Math.min(dp[i], dp[i - j*j] + 1);				
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}

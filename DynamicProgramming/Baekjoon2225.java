7d7a11343a3a7d396b723b8f1f7c2d9e64e728d1
gooriiie <fkstldghks123@naver.com>
2022-01-28 15:33:00 +0900

    Dynamic Programming : BOJ11048

--- DynamicProgramming/Baekjoon2225.java
+++ DynamicProgramming/Baekjoon2225.java
@@ -17,14 +17,14 @@ public class Baekjoon2225 {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
-		int[][] dp = new int[N+1][K+1];
+		long [][] dp = new long[N+1][K+1];
 		
 		for(int i = 0; i < N+1; i++) {
 			dp[i][0] = 0;
 			dp[i][1] = 1;
 		}
 		
-		for(int j = 1; j < K+1; j++) {
+		for(int j = 2; j < K+1; j++) {
 			for(int i = 0; i < N+1; i++) {
 				for(int h = 0; h <= i; h++) {
 					dp[i][j] += dp[h][j-1] % 1000000000;
